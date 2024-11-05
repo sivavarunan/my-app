3cd9c7e6ceb37cfab159dea607889f6ccdb40288
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-05 17:30:42 +0530

    created a circular spining loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+// components/Loading.js
 
-const loading = () => {
+import React from 'react';
+
+const Loading = () => {
   return (
-    <div>____________</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 border-4 border-t-transparent border-blue-500 border-solid rounded-full animate-spin"></div>
+        <span className="text-lg font-semibold text-gray-700">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
-export default loading
+export default Loading;
