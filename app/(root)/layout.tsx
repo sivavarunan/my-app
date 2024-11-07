7a1c3fae3917558c10bd0bc968ca43e5e7a5b7b3
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-07 09:22:41 +0530

    demo-app started

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,7 @@
-import React from "react"
-const layout = ({children}:{children :React.ReactNode}) => {
-  return (
-    <div>
-        <p className="text-3xl"> NAVBAR</p>
-        {children}
-    </div>
-  )
+export default function Layout({children}: Readonly<{children: React.ReactNode}>){
+    return (
+        <main className="font-work-sans">
+            {children}
+        </main>
+    )
 }
-
-export default layout
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import Hello from "../components/hello";
-export default function Home() {
-  return (
-    <>
-   <h1 className="text-3xl">Welcome to nextjs</h1>
-   <Hello/>
-   </>
-  );
+export default function Home(){
+    return (
+        <>
+        HOME
+        </>
+    )
 }
