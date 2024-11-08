a88aa047735eafcce21dab4a0aec3161bf61db4f
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-08 23:56:45 +0530

    authentication done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,21 +17,25 @@ const Navbar = async() => {
                     <Link href="/startup/create">
                         <span>Create</span>
                     </Link>
-                    <button onClick={signOut}>
-                        <span>
-                            Logout
-                        </span>
-                    </button>
+                    <form action={async () => {
+                        "use server";
+                        await signOut({redirectTo:"/"});
+
+                    }}>
+                      <button type='submit'>
+                        Logout
+                        </button>
+                    </form>
                     <Link href="{`/user/${session?.id}">
                     <span>{session?.user?.name}</span>
                     </Link>
                     </>
                 ):(
-                    <button onClick={async () => {
+                    <form action={async () => {
                         "use server";
                         await signIn('github')}}>
-                        <span>Login</span>
-                    </button>
+                        <button type='submit'>Login</button>
+                    </form>
 
                 )}
             </div>
