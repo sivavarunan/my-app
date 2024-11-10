47db28daf1b7c9fe3a01710ba3a25aee7c55143b
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-10 23:33:15 +0530

    SearchForm.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
-export default function Home(){
-    return (
-      <>
-      <h1 className="heading">Home</h1>
-      </>
-    )
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, <br/> Connect with enterprenuers</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submut Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+        </p>
+        <SearchForm />
+      </section>
+    </>
+  )
 }
+
+
