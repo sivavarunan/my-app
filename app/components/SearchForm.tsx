6b9200f4069b2511b1e4315edfbf9fd337deaeed
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-11 16:56:30 +0530

    SearchFormReset.tsx

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import Form from 'next/form'
+import SearchFormReset from './SearchFormReset';
 
 const SearchForm = () => {
     const query = "Test";
 
-    const reset = () => {
-        const form = document.querySelector('.search-form') as HTMLFormElement;
-
-        if(form) form.reset();
-    }
+    
   return (
     <Form action="/" scroll={false} className='search-form'>
         <input name="query"
@@ -19,9 +16,8 @@ const SearchForm = () => {
 
         <div className='flex gap-2'>
             {query && (
-                <button type='reset' onClick={reset}>
-
-                </button>
+                <SearchFormReset />
+               
             )}
         </div>
 
