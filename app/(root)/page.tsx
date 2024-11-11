6b1d27336323b5abda6ec16f936be2c8adb96dcd
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-11 17:03:42 +0530

    searching query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams: Promise<{ query?: string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -8,7 +11,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submut Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
         </p>
-        <SearchForm />
+        <SearchForm query={query}/>
       </section>
     </>
   )
--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import Form from 'next/form'
 import SearchFormReset from './SearchFormReset';
 
-const SearchForm = () => {
-    const query = "Test";
-
-    
+const SearchForm = ({query}:{query?: string}) => {
+ 
   return (
     <Form action="/" scroll={false} className='search-form'>
         <input name="query"
@@ -15,10 +13,12 @@ const SearchForm = () => {
         />
 
         <div className='flex gap-2'>
-            {query && (
+            {query && 
                 <SearchFormReset />
-               
-            )}
+            }
+            <button type='submit' className='search-btn text-white'>
+              S
+            </button>
         </div>
 
     </Form>
