23a23698d6250d87fc4f963867b273497a7387d8
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-12 22:08:32 +0530

    updated search bar components

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,19 +1,21 @@
-import Link from 'next/link';
-import React from 'react'
+"use client"
+
+import Link from "next/link";
+import {X} from "lucide-react";
 
 const SearchFormReset = () => {
-  const reset = () => {
-    const form = document.querySelector('.search-form') as HTMLFormElement;
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
 
-    if (form) form.reset();
-  }
-  return (
+        if(form) form.reset();
+    }
 
-    <button type='reset' onClick={reset}>
-      <Link href="/" className='search-btn text-white' >X
-      </Link>
-    </button>
-  )
+    return (
+        <button type="reset" onClick={reset}>
+            <Link href="/" className="search-btn text-white">
+                <X className="size-5" />
+            </Link>
+        </button>
+    )
 }
-
 export default SearchFormReset
