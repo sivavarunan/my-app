9281b88287003b00600cf19c927647b741a50b11
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-14 05:27:04 +0530

    implemented startup card ui

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,11 +1,13 @@
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
-  const { _createdAt, views, author: { id: authorID, name }, title, category, _id, image } = post;
+  const { _createdAt, views, author: { id: authorID, name }, title, category, _id, image, description } = post;
 
   return (
     <li className='startup-card group'>
@@ -26,12 +28,29 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             </p>
           </Link>
           <Link href={`/startup/${_id}`}>
-          <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+            <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
           </Link>
         </div>
+        <Link href={`/user/${authorID}`}>
+          <Image src="https://placehold.co/48x48" alt='PLACEHOLDER' width={48} height={48} className='rounded-full'>
+          </Image>
+        </Link>
       </div>
-    </li>
+      <Link href={`/startup/${_id}`}>
+        <p className='startup-card_desc'>{description}</p>
+        <img src={image} alt='placeholder' className='startup-card_image' />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+
+    </li >
   )
-}
+};
 
 export default StartupCard
