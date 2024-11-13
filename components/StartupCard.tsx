c7a4b4abec07058bc2ed1bab0f3aec30e864608a
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-14 01:21:35 +0530

    ...

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,16 +1,35 @@
 import { formatDate } from '@/lib/utils'
+import { EyeIcon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
-const StartupCard = ({post}:{post: StartupTypeCard}) => {
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+
+  const { _createdAt, views, author: { id: authorID, name }, title, category, _id, image } = post;
+
   return (
     <li className='startup-card group'>
       <div className='flex-between'>
         <p className='startup_card_date'>
-          {formatDate(post._createdAt)}
+          {formatDate(_createdAt)}
         </p>
-
+        <div className='flex gap-1.5'>
+          <EyeIcon className='size-6 text-primary' />
+          <span className='text-16-medium'>{views}</span>
+        </div>
       </div>
-
+      <div className='flex-between mt-5 gap-5'>
+        <div className='flex-1'>
+          <Link href={`/user/${authorID}`}>
+            <p className='text-16-medium line-clamp-1'>
+              {name}
+            </p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+          <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+          </Link>
+        </div>
+      </div>
     </li>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(date: string){
-  return new Date(date).toLocaleDateString('en-US',{month: 'long',day:'numeric',year:'numeric'})
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
 }
