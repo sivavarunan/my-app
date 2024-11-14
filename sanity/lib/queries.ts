e0cb095ea42eae89d32824258baddf3b802c8964
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-15 03:05:55 +0530

    display the fetched data in home page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,6 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)]{
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
   _id,
   title,
   slug,
