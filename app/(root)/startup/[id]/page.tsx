34d0714cfd5a01a26190867545fbc785a5dee067
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-17 00:39:06 +0530

    details page 2

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,13 +1,26 @@
-import { promises } from 'dns'
+import { formatDate } from '@/lib/utils';
+import { client } from '@/sanity/lib/client';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const page = async ({params}:{params: Promise<{ id: string}>} ) => {
-    const id = (await params).id;
+export const experimental_ppr = true;
+
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  if (!post) return notFound();
   return (
     <>
-    <h1 className='text-3xl'>
-        This is a startup number: {id}
-    </h1>
+      <section className='pink_container !min-h-[230px]'>
+        <p className='tag'>{formatDate(post?._createdAt)}
+          </p> 
+          </section>
+        <h1 className='text-3xl'>
+          {post.title}
+        </h1>
+     
     </>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,17 @@ const nextConfig: NextConfig = {
     remotePatterns: [{
       protocol: 'https',
       hostname: "*",
-    }]
-  }
+    },
+  ],
+  },
+  // experimental: {
+  //   ppr: "incremental",
+  //   after: true,
+  // },
+  // devIndicators: {
+  //   appIsrStatus: true,
+  //   buildActivity: true,
+  //   buildActivityPosition: "bottom-right",
+  // },
 };
-
 export default nextConfig;
