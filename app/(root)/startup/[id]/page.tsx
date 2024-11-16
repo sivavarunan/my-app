f102b6647963d0bc604b85bd74c670b631e7586a
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-17 01:14:37 +0530

    markdown typesafety

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,7 +2,11 @@ import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import markdownit from 'markdown-it'
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
@@ -11,16 +15,43 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
   if (!post) return notFound();
+
+  const parsedContent = md.render(post?.pitch || '');
+
   return (
     <>
       <section className='pink_container !min-h-[230px]'>
-        <p className='tag'>{formatDate(post?._createdAt)}
-          </p> 
-          </section>
-        <h1 className='text-3xl'>
+        <p className='tag'>
+          {formatDate(post?._createdAt)}
+        </p> 
+         <h1 className='heading'>
           {post.title}
         </h1>
-     
+        <p className='sub-heading max-w-5xl'>{post.description}</p> 
+      </section>
+
+      <section className='section_container'>
+        <img src={post.image} alt='thumbnail' className='w-full h-auto rounded-xl' />
+
+        <div className='space-y-5 mt-10 max-w-4xl mx-auto'>
+          <div className='flex-between gap-5'>
+            <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
+            <Image
+            src={post.author.image}
+            alt="avatar"
+            width={64}
+            height={64}
+            className="rounded-full drop-shadow-lg" />
+
+            <p className='text-20-medium'>{post.author.name}</p>
+            <p className='text-16-medium !text-black-300'>@{post.author.name}</p>
+        
+            </Link>
+            <p className='category-tag'>{post.category}</p>
+          </div>
+          <h3 className='text-30-bold'>Pitch Details</h3>
+        </div>
+      </section>
     </>
   )
 }
