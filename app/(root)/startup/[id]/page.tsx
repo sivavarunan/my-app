c13e444ed7a75c82ea47e40efa3e1456a5cbcce4
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-17 08:50:41 +0530

    skeketon

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
@@ -5,6 +6,8 @@ import { notFound } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import markdownit from 'markdown-it'
+import { Suspense } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const md = markdownit();
 
@@ -50,8 +53,19 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
             <p className='category-tag'>{post.category}</p>
           </div>
           <h3 className='text-30-bold'>Pitch Details</h3>
+          {parsedContent ? (
+            <article 
+            className='prose max-w-4xl font-work-sans break-all'
+            dangerouslySetInnerHTML={{ __html: parsedContent}} />
+          ) : (<p className='no-result'>
+            No details provided
+          </p>)}
         </div>
+        <hr className='divider'/>
       </section>
+      <Suspense fallback={<Skeleton className='view_skeleton'/>}>
+
+      </Suspense>
     </>
   )
 }
