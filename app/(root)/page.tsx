b8c99b695c0cbdf5e445442b6fd05f250b82d9e7
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-17 12:34:39 +0530

    auth..

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
-import { AuthError } from "next-auth";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "../auth";
 
 
 export default async function Home({ searchParams }: {
@@ -11,6 +11,10 @@ export default async function Home({ searchParams }: {
 
   const query = (await searchParams).query;
   const params = {search: query || null};
+
+  const session = await auth();
+  console.log(session?.id);
+
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
   return (
