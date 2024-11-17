6be9d66daf8b18970d11b29c0839bdb698a747ce
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-11-17 12:23:18 +0530

    auth redefined by adding tokens and sessions

--- app/auth.ts
+++ app/auth.ts
@@ -1,6 +1,39 @@
+import { client } from "@/sanity/lib/client"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
+import { writeClient } from "@/sanity/lib/write-client"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
-})
+  callbacks:{
+
+    async signIn({user: {name,email, image}, profile : {id, login, bio },}){
+      const exsistingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {id});
+    
+    if (!exsistingUser) {
+      await writeClient.create({
+        _type: 'author',
+        id,
+        name,
+        username: login,
+        email,
+        image,
+        bio: bio || '',
+      });
+    }
+    return true;
+  },
+  async jwt({token, account, profile}){
+    if(account && profile) {
+      const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id:profile?.id,});
+      token.id = user._id;
+    }
+    return token;
+  },
+  async session({session, token}){ 
+    Object.assign(session,{id: token.id});
+    return session;
+  },
+
+}});
